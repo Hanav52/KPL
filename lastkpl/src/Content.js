3790b49fd956ad6d57b277b15e6e44e35c6fa2a9
Hanav52 <wnsrl8329@gmail.com>
2022-10-05 03:05:13 +0900

    파일 경로 수정

--- lastkpl/src/Content.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-
-export default function Content() {
-    return (
-        <BrowserRouter>
-        </BrowserRouter>
-    )
-}
