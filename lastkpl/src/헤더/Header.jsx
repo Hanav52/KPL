c99c64813241d39f99f5c8ffd9903a6ad870b481
Hanav52 <wnsrl8329@gmail.com>
2022-10-06 02:09:22 +0900

    헤더 부분 localStorage로 카테고리 id, name 저장

---
+++ lastkpl/src/헤더/Header.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Hcompo from './HeaderMenu';
+
+export default function HeaderMenu() {
+    return (
+        <>
+            <Hcompo/>
+        </>
+    )
+}
