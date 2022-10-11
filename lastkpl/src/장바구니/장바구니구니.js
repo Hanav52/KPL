9ef0b6aeba4e24c8d7d587f54f9be89f6581080c
Hanav52 <wnsrl8329@gmail.com>
2022-10-12 00:00:25 +0900

    장바구니 디자인

---
+++ lastkpl/src/장바구니/장바구니구니.js
@@ -0,0 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
+
+export default function 장바구니구니 () {
+    return (
+        <BrowserRouter>
+            <div className="contentWrapper">
+                <div className="contentWrap">
+                    <div className="content">
+                        <div className="shop_detail" id="productDetail">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </BrowserRouter>
+    )
+}
