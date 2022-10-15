6041357c7caa067c5063abd816bdd61cd58d8d47
Hanav52 <wnsrl8329@gmail.com>
2022-10-15 20:31:48 +0900

    스택 큐 공부

--- lastkpl/src/Redux/1_스택과_큐.js
+++ lastkpl/src/Redux/1_스택과_큐.js
@@ -6,6 +6,27 @@ class Stack {
     push(data) {
         this.arr.push(data);
     }
+
+    pop(index) {
+        if(index === this.arr.length-1) {
+            return this.arr.pop();
+        }
+
+        let result = this.arr.splice(index, 1);
+        return result;
+    }
+
+    empty() {
+        if(this.arr.length === 0) {
+            return true;
+        } else {
+            return false
+        }
+    }
+
+    top() {
+        return this.arr[this.arr.length]
+    }
 }
 
 let s = new Stack();
@@ -16,5 +37,6 @@ s.push(100);
 s.push(200);
 s.push(300);
 
+let popValue = s.pop(2)
 console.log(s);
 console.log(s.arr[0])
