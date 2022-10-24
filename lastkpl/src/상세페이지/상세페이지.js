f461bc2997c59db9f59569ec55438e065b051e32
Hanav52 <wnsrl8329@gmail.com>
2022-10-24 23:28:14 +0900

    상세페이지 api 연결

--- lastkpl/src/상세페이지/상세페이지.js
+++ lastkpl/src/상세페이지/상세페이지.js
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
+import { getClothAPI } from "../API";
 import BOT from "./상세페이지BOT";
 import MID from "./상세페이지MID";
 import TOP from "./상세페이지TOP";
 
 export default function DetailPage () {
+
+// api 호출
+  const ClothAPI = async () => {
+    try {
+      const response = await getClothAPI(localStorage.getItem("b_num"));
+      console.log(response);
+      setRes(response)
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    ClothAPI();
+  }, [])
+
+  const [res, setRes] = useState();
+
     return (
         <BrowserRouter>
             <div className="contentWrapper">
