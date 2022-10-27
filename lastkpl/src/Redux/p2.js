9a41cabe05e792ade2ed4b919f1343b695f95af4
Hanav52 <wnsrl8329@gmail.com>
2022-10-28 00:06:27 +0900

    알고리즘 2

--- lastkpl/src/Redux/p2.js
+++ lastkpl/src/Redux/p2.js
@@ -1,10 +1,25 @@
-function problem2(cryptogram) {
-    var answer = "";
-    for (let i = 0; i < cryptogram.length; i++) {
-      console.log(cryptogram[i], i, cryptogram.indexOf(cryptogram[i]));
+function solution(arr) {
+  var answer = [...arr[0]];
+  var abc;
+
+  for (var i = 1; i < arr.length; i++) {
+    var top = answer[answer.length - 1];
+    if (top !== arr[i]) {
+      answer.push(arr[i]);
+      abc = arr[i];
+    } else if (top === arr[i]) {
+      //abc = arr[i];
+      answer.pop(arr[i]);
+      console.log(abc);
+      if (abc !== arr[i]) {
+        console.log("1");
+        answer.pop(arr[i]);
+      }
+    } else if (abc === top) {
+      console.log("dhkdn");
     }
-    return answer;
   }
-  
-  module.exports = problem2;
-  
+  return answer;
+}
+
+console.log(solution("browoanoommmnaon"));
