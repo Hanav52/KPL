bbb5f75adedcc7e84c8b4b73436b763db03098a8
Hanav52 <wnsrl8329@gmail.com>
2022-10-31 23:50:51 +0900

    6번 알고리즘

---
+++ lastkpl/src/Redux/p6.js
@@ -0,0 +1,8 @@
+const arr = [
+    ["jm@email.com", "제이엠"],
+    ["jason@email.com", "제이슨"],
+    ["woniee@email.com", "워니"],
+    ["mj@email.com", "엠제이"],
+    ["nowm@email.com", "이제엠"],
+  ]
+const set = new Set(arr)
