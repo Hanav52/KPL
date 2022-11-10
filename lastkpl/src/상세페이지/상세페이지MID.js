4337c96ab915410516dbd5606c3a704c529cd0d3
Hanav52 <wnsrl8329@gmail.com>
2022-11-10 23:39:18 +0900

    상세페이지 중간 부분 구현

--- lastkpl/src/상세페이지/상세페이지MID.js
+++ lastkpl/src/상세페이지/상세페이지MID.js
@@ -1,7 +1,72 @@
-export default function MID () {
-    return (
-        <div className="product_slide_box">
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { useState } from 'react';
+import MidDetail from './detailMidDetail';
+import ProductComment from './ProductComment';
 
-        </div>
-    )
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+Mid.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function Mid({resimage}) {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%', padding: '12rem' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" centered>
+          <Tab label="상세페이지" {...a11yProps(0)} />
+          <Tab label="상품후기" {...a11yProps(1)} />
+          <Tab label="상품문의" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <MidDetail resimage={resimage}/>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <ProductComment/>
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        상품문의
+      </TabPanel>
+    </Box>
+  );
 }
