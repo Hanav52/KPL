9be54e6e03d328b107211ca8f9f337eab63c602f
Hanav52 <wnsrl8329@gmail.com>
2022-11-12 23:29:45 +0900

    댓글 정렬 기능 추가

--- lastkpl/src/상세페이지/상세페이지.js
+++ lastkpl/src/상세페이지/상세페이지.js
@@ -1,9 +1,32 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
-import { getClothAPI } from "../API";
+import { getClothAPI, getCommentAPI } from "../API";
 import BOT from "./상세페이지BOT";
 import MID from "./상세페이지MID";
 import TOP from "./상세페이지TOP";
+import '../RealMain.css'
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+// redux
+function reducer(currentState, action) {
+  if(currentState === undefined){
+    return {
+      sort: "c_time,desc",
+      categoryid: 0,
+    }
+  }
+  const newSort = {...currentState};
+
+  if(action.type === '최신순') {
+    newSort.sort = 'c_time,desc'
+  }
+  else if(action.type === '오래된순') {
+    newSort.sort = 'c_time,asc'
+  }
+  return newSort;
+}
+const store = createStore(reducer);
 
 export default function DetailPage () {
 
@@ -11,19 +34,26 @@ export default function DetailPage () {
   const ClothAPI = async () => {
     try {
       const response = await getClothAPI(1);
-      console.log(response.data.data1[0].detailpicture_url);
       setRes(response.data.data1)
       setResImage(response.data.data1[0].detailpicture_url)
-    //   for(let a = 0; a < response.data.data1.length; a++) {
-    //     setResImage(response.data.data1[0].detailpicture_url[a].url)
-    // };
+    } catch (e) {
+      console.error(e);
+    }
+  };
+// 댓글 api 호출
+  const CommentAPI = async () => {
+    try {
+      const response = await getCommentAPI(1, 1, 10, "c_time,desc");
+      console.log(response)
     } catch (e) {
       console.error(e);
     }
   };
 
+
   useEffect(() => {
     ClothAPI();
+    CommentAPI();
   }, [])
 
   const [res, setRes] = useState([]);
@@ -31,17 +61,19 @@ export default function DetailPage () {
 
     return (
         <BrowserRouter>
+          <Provider store={store}>
             <div className="contentWrapper">
                 <div className="contentWrap">
                     <div className="content">
                         <div className="shop_detail" id="productDetail">
                             <TOP res={res} resimage={resimage}/>
-                            <MID/>
+                            <MID resimage={resimage}/>
                             <BOT/>
                         </div>
                     </div>
                 </div>
             </div>
+          </Provider>
         </BrowserRouter>
     )
 }
