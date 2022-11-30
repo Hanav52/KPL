a04ff64805c0f2eda34dd9db8666b8eb0b24aaa5
Hanav52 <wnsrl8329@gmail.com>
2022-11-30 16:58:07 +0900

    프로필 구조 만들기

---
+++ lastkpl/src/프로필/CenterProfile.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { getClothAPI, getCommentAPI } from "../API";
+import '../RealMain.css'
+
+export default function CenterProfile () {
+
+    return (
+        <>
+            <div>
+                본문
+            </div>
+        </>
+    )
+}
---
+++ lastkpl/src/프로필/SideProfile.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { getClothAPI, getCommentAPI } from "../API";
+import '../RealMain.css'
+
+export default function SideProfile () {
+
+    return (
+        <>
+            <div>
+                MyProfile
+            </div>
+        </>
+    )
+}
