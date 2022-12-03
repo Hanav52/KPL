42e8d53ed5c8e130450aef144ef93b6cb051868a
Hanav52 <wnsrl8329@gmail.com>
2022-12-03 15:52:55 +0900

    토큰 재발급 기능

--- lastkpl/src/RefreshToken.js
+++ lastkpl/src/RefreshToken.js
@@ -1,14 +1,36 @@
-import { useEffect } from "react";
-import { getRefreshToken } from "./API";
+import { getRefreshToken } from './API';
+import moment from 'moment';
 
-const RefreshToken = async () => {
-  try {
-    const response = await getRefreshToken();
-  } catch (e) {
-    console.error(e);
-  }
-};
 
-useEffect(() => {
-  RefreshToken();
-}, []);
+export const GetRefreshToken = async (e) => {
+
+    const data = {
+          refreshToken: localStorage.getItem("RefreshToken")
+      }
+    const t2 = new Date();
+    const t1 = Number(localStorage.getItem("AccessTokenExpiresIn"));
+    const diff2 = moment.duration(t1-t2).asMilliseconds()  // 400000이하로 떨어지면
+                
+        try {
+            if(diff2 < 400000) {
+                console.log("곧 만료")
+                const response = await getRefreshToken(data);
+                console.log(response)
+                localStorage.removeItem("AccessToken");
+                localStorage.removeItem("AccessTokenExpiresIn");
+                localStorage.removeItem("RefreshToken");
+                localStorage.removeItem("RefreshTokenExpiresIn");
+                localStorage.setItem("AccessToken", response.data.accessToken);
+                localStorage.setItem("AccessTokenExpiresIn", response.data.accessTokenexpiresIn);
+                localStorage.setItem("RefreshToken1", response.data.refreshToken);
+                localStorage.setItem("RefreshTokenExpiresIn", response.data.refreshTokenexpiresIn);
+                localStorage.setItem("openmodal", true);
+              } else {
+              // 글 작성하는 곳으로 이동
+              console.log("아직 유효함")
+              localStorage.setItem("openmodal", true);
+              }
+            } catch (e) {
+                console.error(e);
+             }
+}
