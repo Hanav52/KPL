2f3a47f6ad2409cc00cff2c32d3e77cfebc88e93
Hanav52 <wnsrl8329@gmail.com>
2022-12-04 23:45:09 +0900

    프로필 구조 틀 구현

--- lastkpl/src/프로필/CenterProfile.js
+++ lastkpl/src/프로필/CenterProfile.js
@@ -1,15 +1,35 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
+import styled from "styled-components";
 import { getClothAPI, getCommentAPI } from "../API";
 import '../RealMain.css'
+
+const Div = styled.div`
+  width: 70%; /* 가로 사이즈가 1200px 넘는 해상도에서는 max-width 설정 */
+  height: 100%; /* align-items 등으로 수직 가운데 정렬을 위해, 알맞은 height 설정이 필요함 */
+  padding: 0rem 4rem; /* 좌우 padding 을 살짝 주어서, 좌우에 딱 붙지 않도록 함 */
+`;
 
 export default function CenterProfile () {
 
     return (
-        <>
-            <div>
-                본문
+        <Div>
+            <div className="profilecontent">
+                <div className="mypage">
+                    <div className="section1">
+                        <h2>회원정보</h2>
+                        <div className="group">
+                            <form className="formtest">
+                                <input placeholder="이름"/>
+                                <input placeholder="아이디"/>
+                                <input placeholder="성별"/>
+                                <input placeholder="키"/>cm
+                                <input placeholder="몸무게"/>kg
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </>
+        </Div>
     )
 }
--- lastkpl/src/프로필/SideProfile.js
+++ lastkpl/src/프로필/SideProfile.js
@@ -1,15 +1,65 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 import { getClothAPI, getCommentAPI } from "../API";
 import '../RealMain.css'
+import styled from "styled-components";
+
+const Div = styled.div`
+  width: 30%; /* 가로 사이즈가 1200px 넘는 해상도에서는 max-width 설정 */
+  height: 100%; /* align-items 등으로 수직 가운데 정렬을 위해, 알맞은 height 설정이 필요함 */
+  padding: 0rem 2rem; /* 좌우 padding 을 살짝 주어서, 좌우에 딱 붙지 않도록 함 */
+`;
+const DD = styled.dd`
+margin-bottom: 50px
+`;
+const Li = styled.li`
+margin-bottom: 5px;
+`;
+const Ul = styled.ul`
+padding: 0;
+margin: 0;
+`;
 
 export default function SideProfile () {
 
     return (
-        <>
-            <div>
-                MyProfile
-            </div>
-        </>
+            <Div>
+                <div className="aside">
+                    <div className="asideMenu">
+                        <div className="mypage">
+                            <h2>마이페이지</h2>
+                            <dl>
+                                <DD>
+                                    <h3>쇼핑정보</h3>
+                                    <Ul>
+                                        <Li>
+                                            <Link className="pagea">장바구니</Link>
+                                        </Li>
+                                    </Ul>
+                                </DD>
+                                <DD>
+                                    <h3>고객센터</h3>
+                                    <Ul>
+                                        <Li>
+                                            <Link className="pagea">문의</Link>
+                                        </Li>
+                                    </Ul>
+                                </DD>
+                                <DD>
+                                    <h3>회원정보</h3>
+                                    <Ul>
+                                        <Li>
+                                            <Link className="pagea">회원정보 변경</Link>
+                                        </Li>
+                                        <Li>
+                                            <Link className="pagea">회원탈퇴</Link>
+                                        </Li>
+                                    </Ul>
+                                </DD>
+                            </dl>
+                        </div>
+                    </div>
+                </div>
+            </Div>
     )
 }
