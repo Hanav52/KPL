c9285f86fe76fb16d71cd1ccbe0b15839c5a0e1d
Hanav52 <wnsrl8329@gmail.com>
2022-12-16 23:51:42 +0900

    코드 수정

--- lastkpl/src/index.js
+++ lastkpl/src/index.js
@@ -6,9 +6,7 @@ import SignIn from './로그인&회원가입/Login';
 import reportWebVitals from './reportWebVitals';
 import DetailPage from './상세페이지/상세페이지';
 import Bascket from './장바구니/장바구니';
-
-
-
+import MyProfile from './프로필/MyProfile';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,7 +15,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
